9d4ebcf1e519322d93d789dc5ddcdb88e1209e64
star <star@stardeMacBook-Pro.local>
2015-05-17 11:04:16 +0800

    Initial Commit

---
+++ SHWTests/SHWTests.swift
@@ -0,0 +1,36 @@
+//
+//  SHWTests.swift
+//  SHWTests
+//
+//  Created by star on 15/5/17.
+//  Copyright (c) 2015年 star. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SHWTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
